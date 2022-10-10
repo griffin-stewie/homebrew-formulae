bd2de04121b44a76c612050d434bc5398a78f0a3
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-10 16:44:45 +0900

    my_echo 0.0.18
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/my_echo.rb
+++ Formula/my_echo.rb
@@ -4,8 +4,8 @@
 class MyEcho < Formula
   desc "my_echo command is just sample code."
   homepage "https://github.com/griffin-stewie/my_echo"
-  url "https://github.com/griffin-stewie/my_echo/releases/download/v0.0.17/release_binary.zip"
-  sha256 "8b7eade752c4536daa4d6a944a631b1301a01a06219e4110cfc7d82ce5347356"
+  url "https://github.com/griffin-stewie/my_echo/releases/download/v0.0.18/release_binary.zip"
+  sha256 "6baef9ce64b56ad2827383659400fdfb026c5a1bcdab168ef579e9b68158756a"
   license "MIT"
 
   def install
