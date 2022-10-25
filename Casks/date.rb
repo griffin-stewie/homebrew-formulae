84309a6587116c8879abc1f50c832880298b8612
griffin-stewie <panterathefamilyguy@gmail.com>
2022-10-25 22:52:52 +0900

    Update date.rb

--- Casks/date.rb
+++ Casks/date.rb
@@ -1,5 +1,5 @@
 cask "date" do
-  version "1.0.2"
+  version "0.1"
   sha256 "10b6c0d7bd86b6f063ee58bff34a67ddde6a8b487bcf23fc1d7abe25bc05418a"
   
   url "https://github.com/griffin-stewie/Date/releases/download/v#{version}/Date.app.zip"
