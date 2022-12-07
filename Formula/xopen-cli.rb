9f8142d19693255b4ceadcd23187f28702814cce
griffin-stewie <panterathefamilyguy@gmail.com>
2022-12-08 00:38:48 +0900

    xopen-cli 1.8.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/xopen-cli.rb
+++ Formula/xopen-cli.rb
@@ -4,8 +4,8 @@
 class XopenCli < Formula
   desc "xopen command helps you open your project using Xcode."
   homepage "https://github.com/griffin-stewie/xopen-cli"
-  url "https://github.com/griffin-stewie/xopen-cli/releases/download/v1.8.4/release_binary.zip"
-  sha256 "a756bec07167e8e727421b36dec49d4bf6394607c46eb1f4eb1c37ea3bee2a70"
+  url "https://github.com/griffin-stewie/xopen-cli/releases/download/v1.8.6/release_binary.zip"
+  sha256 "ce5b079f6031cdc4b5ba9277d28fe217991d99932810ed3ba96aeba6c364f077"
   license "MIT"
 
   def install
