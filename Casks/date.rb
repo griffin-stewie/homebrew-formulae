bec4892a1d1312f3146bea679f041b8026d327f5
griffin-stewie <panterathefamilyguy@gmail.com>
2023-01-16 20:18:20 +0900

    date 1.0.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Casks/date.rb
+++ Casks/date.rb
@@ -1,8 +1,8 @@
 cask "date" do
-  version "1.0.0"
-  sha256 "82062da448661ace520b37e06cc3b0783e0b26643ecbf3cbb3d33d317ffefbeb"
+  version "1.0.2"
+  sha256 "49af239933eccff9a375c373c0f89f6ea029a77788ecc330c48f7833fd48d446"
   
-  url "https://github.com/griffin-stewie/Date/releases/download/v#{version}/Date.app.zip"
+  url "https://github.com/griffin-stewie/Date/releases/download/v1.0.2/Date.app.zip"
   name "Date"
   desc "Provides Widget shows simple date"
   homepage "https://github.com/griffin-stewie/Date"
